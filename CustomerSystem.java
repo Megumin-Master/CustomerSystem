0c8c2001348ceb5a556f3369a94d8366ebf552b7
David <david4han@gmail.com>
2021-04-07 15:25:05 -0400

    Naomi's DrJava Code

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -1,4 +1,4 @@
-package luhn;
+package Luhn;
 
 // Throughout this project, the use of data structures are not permitted such as methods like .split and .toCharArray
 
@@ -27,7 +27,7 @@ class CustomerSystem{
       if (userInput.equals(enterCustomerOption)){
         // Only the line below may be editted based on the parameter list and how you design the method return
         // Any necessary variables may be added to this if section, but nowhere else in the code
-        enterCustomerInfo();
+        enterCustomerInfo(reader);
         
         /* The following is a test to make sure that the credit card validation works as planned. */
         String creditCard = validateCreditCard(reader);
@@ -61,7 +61,43 @@ class CustomerSystem{
    * The method may not nesessarily be a void return type
    * This method may also be broken down further depending on your algorithm
    */
-  public static void enterCustomerInfo() {
+  public static void enterCustomerInfo(Scanner reader) {
+    String firstName; // user enters first name
+    System.out.println("Please enter your first name: ");
+    
+    firstName = reader.nextLine();
+    
+    // seperating strings
+    
+    String lastName; // user enters last name
+    System.out.println("Please enter your last name: ");
+    
+    lastName = reader.nextLine();
+    
+    // seperating strings
+    
+    String city; // user enters their city
+    System.out.println("Please enter which city you live in: ");
+    
+    city = reader.nextLine();
+    
+    // seperating strings
+    
+    String postalCode; // user enters their postal code
+    System.out.println("Please enter your postal code: ");
+    
+    postalCode = reader.nextLine();
+    
+    System.out.println("First name: " + firstName);
+    System.out.println("Last name: " + lastName);
+    System.out.println("City: " + city);
+    System.out.println("Postal code: " + postalCode);
+    System.out.println();
+    
+    //creating the assigned value
+    String file = "customerData.csv";
+    String line = "";
+    
   }
   /*
    * This method may be edited to achieve the task however you like.
@@ -138,7 +174,7 @@ class CustomerSystem{
       // 3.b) Sum the partial sums of the even digits to form sum2
       sum2 = sum2 + dbl;
     }
-
+    
     // 4. If sum1 + sum2 ends in zero then the original number is valid, otherwise it is invalid.
     int total = sum1 + sum2;
     if (total % 10 == 0) {
